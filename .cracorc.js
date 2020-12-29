fb87d01548330fc1220906bfdff27cf091301687
Gu-Miao <1450206741@qq.com>
2020-12-29 12:57:40 +0800

    Add @babel/plugin-proposal-decorators and babel-import-plugin.

--- .cracorc.js
+++ .cracorc.js
@@ -3,7 +3,12 @@ const path = require('path')
 const resloveSrc = (...paths) => path.join(__dirname, 'src', ...paths)
 
 module.exports = {
-  babel: {},
+  babel: {
+    plugins: [
+      ['import', { libraryName: 'antd', style: 'css' }],
+      ['@babel/plugin-proposal-decorators', { legacy: true }]
+    ]
+  },
   webpack: {
     alias: {
       '@': resloveSrc(),
