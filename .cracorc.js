b8fd82865f6d21eb0750df5e5c7555825f931b5e
Gu-Miao <1450206741@qq.com>
2020-12-29 12:42:45 +0800

    Change paths alias.

--- .cracorc.js
+++ .cracorc.js
@@ -1,11 +1,12 @@
 const path = require('path')
 
-const resloveSrc = _path => path.join(__dirname, 'src', _path)
+const resloveSrc = (...paths) => path.join(__dirname, 'src', ...paths)
 
 module.exports = {
   babel: {},
   webpack: {
     alias: {
+      '@': resloveSrc(),
       '@assets': resloveSrc('assets'),
       '@components': resloveSrc('components'),
       '@pages': resloveSrc('pages'),
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import logo from '@assets/log.svg'
+import logo from '@assets/logo.svg'
 
 import './style.scss'
 
